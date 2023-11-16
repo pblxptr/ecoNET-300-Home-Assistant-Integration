f158192a34ba25d7064e639dba9cabff38cd671e
jontofront <37038426+jontofront@users.noreply.github.com>
2023-11-16 10:09:31 +0000

    move constant add doc string

--- custom_components/econet300/binary_sensor.py
+++ custom_components/econet300/binary_sensor.py
@@ -1,3 +1,4 @@
+"""Econet binary sensor"""
 from dataclasses import dataclass
 
 import logging
--- custom_components/econet300/const.py
+++ custom_components/econet300/const.py
@@ -22,3 +22,22 @@ API_SYS_PARAMS_PARAM_HW_VER = "routerType"
 ## Reg params
 API_REG_PARAMS_URI = "regParams"
 API_REG_PARAMS_PARAM_DATA = "curr"
+
+## Boiler staus keys map
+# boiler mode names from  endpoint http://LocalIP/econet/rmParamsEnums?
+OPERATION_MODE_NAMES = {
+    0: "TURNED OFF",
+    1: "FIRE UP",
+    2: "STABILIZATION",
+    3: "WORK",
+    4: "SUPERVISION",
+    5: "BURNING OFF",
+    6: "STOP",
+    7: "R.P.OUT",
+    8: "MANUAL",
+    9: "ALARM",
+    10: "UNSEALING",
+    11: "CHIMNEY",
+    12: "ACTIVACTION",
+    13: "NO TRANSMISSION",
+}
